99680ed9865351f4deab8adc578482de7ad55d94
a3y3 <sohamssd@gmail.com>
2017-12-21 19:37:47 +0530

    Add names to db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+   
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+      }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
 end
