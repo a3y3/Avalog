d673b70bebf57ed02fcc4f09237895690b70e6ac
a3y3 <sohamssd@gmail.com>
2017-12-22 13:19:43 +0530

    Add bootstrap, improve functionality

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -3,7 +3,8 @@ class PlansController < ApplicationController
 		# @plan = current_user.plans.build() -Gives an error when parameters are added
 		@plan = Plan.new(params.require(:plan)
 						.permit(:ambition)
-						.merge(:user_id => current_user.id))
+						.merge(:user_id => current_user.id,
+								:date => Time.now.strftime("%d/%m/%Y")))
 		if(@plan.save)
 			redirect_to root_path
 		else
