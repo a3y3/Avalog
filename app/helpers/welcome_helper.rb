58b886b9dfd299cc45e43aad798684ab9cc62c0f
a3y3 <sohamssd@gmail.com>
2017-12-22 18:37:48 +0530

    Polish frontend

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,17 +1,5 @@
 module WelcomeHelper
 	def display_forms
-		if(@plan.present?) 
-			form_for(@plan) do |f|
-				((f.text_field :accomplishment) +
-			 	(f.submit "Add accomplishment")).html_safe
-			end
-		else
-			form_for(:plan, url: plans_path) do |f| 
-				((Time.now.strftime("%d/%m/%Y")) +
-				
-				(f.text_field :ambition) +
-				(f.submit "Ambition button")).html_safe
-			end
-		end
+		
 	end
 end
